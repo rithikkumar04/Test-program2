b8d531ee3a0aa09b52ac9b17624cf978306b3c31
Rithik Kumar <76579030+rithikkumar04@users.noreply.github.com>
2022-11-22 23:32:09 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+        string a = Console.ReadLine();
+        //Console.WriteLine(a);
+        string b="abcdefghijklmnopqrstuvwxyzabcd";
+        string ans="";
+        for(int i=0;i<a.Length;i++)
+        {
+            for(int j=0;j<b.Length;j++)
+            {
+                if(a[i]==b[j])
+                {
+                    ans=ans+b[j+3];
+                    break;
+                }
+            }
+        }
+        Console.WriteLine(ans);
+    }
+}
